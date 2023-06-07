ddb8fa9957c7e036270c0183ad5f9975b583c68b
ThatColdToast <thatcoldtoast@gmail.com>
2023-06-07 14:44:06 -0500

    cache: no-store

--- app/api/db-edge/route.ts
+++ app/api/db-edge/route.ts
@@ -2,7 +2,7 @@ export const runtime = "edge"
 
 export const GET = async (req: Request) => {
     const start = new Date().getTime()
-    const json = await (await fetch('https://dummyjson.com/users/1', { next: { revalidate: 1 }})).json()
+    const json = await (await fetch('https://dummyjson.com/users/1', { cache: 'no-store' })).json()
     const end = new Date().getTime()
     const diff = end - start
     
